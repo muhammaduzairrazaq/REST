2a15f31886660b5ac133d92dd234a61f324e209d
muhammaduzairrazaq <muhammaduzairrazaq@gmail.com>
2024-02-06 23:10:31 +0500

    basic ecoing of data using django

--- backend/py_client/basic.py
+++ backend/py_client/basic.py
@@ -2,6 +2,9 @@ import requests
 
 endpoint = "https://httpbin.org/status/200"
 endpoint = "https://httpbin.org/anything"
+endpoint = "http://localhost:8000/api/"
 
-get_response = requests.get(endpoint, json={"name":"uzair"})
-print(get_response.text)
+get_response = requests.get(endpoint, params={"password": 123}, json={"query":"Hello world!"})
+# print(get_response.text)
+print(get_response.json())
+# print(get_response.status_code)
